208ec5616d6f002d7166300be65c5fa80f70e050
ZeDaniel06 <silvadan752@gmail.com>
2026-02-04 21:21:14 -0300

    Exclusão e atualização de médicos

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -2,6 +2,7 @@ package med.voll.api.medico;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.*;
 import med.voll.api.endereco.Endereco;
 
@@ -26,12 +27,29 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null)
+            this.nome = dados.nome();
+        if(dados.telefone() != null)
+            this.telefone = dados.telefone();
+        if(dados.endereco() != null)
+            this.endereco.atualizarInformacoes(dados.endereco());
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
